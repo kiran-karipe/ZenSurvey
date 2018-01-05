246aa2853d7c2899ce6e424d41445570eb99a2e7
Kiran <kiran.karipe@gmail.com>
2018-01-05 15:01:28 -0800

    Adding Database

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
@@ -9,8 +11,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
+    User.findOne({ googleId: profile.id})
+      .then((existingUser) => {
+        if(existingUser) {
+          // we already have record with the given profile Id
+        } else {
+          // we don't have user record, make a new record
+          new User({ googleId: profile.id}).save();
+        }
+      });
   })
 );
