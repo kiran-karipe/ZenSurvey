44f40578e6fd914b5646d1ca8cc4edad884d9bc9
Kiran <kiran.karipe@gmail.com>
2018-01-05 18:17:29 -0800

    Finished Auth Flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+//'284656311474-22puun58stuedeikg9v8pn1mek63skrf.apps.googleusercontent.com'
+//'D_En06NBoFANJtfAIZJ4xiWC'
+//'mongodb://kiranprod:password@ds245337.mlab.com:45337/emaily-prodk'
