fe7fe55234cec9e3157dcd2057390c0483c7f6c7
Kiran <kiran.karipe@gmail.com>
2018-01-05 18:32:22 -0800

    Google Redirect Error

--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,6 @@ module.exports = {
 };
 
 
-//'284656311474-22puun58stuedeikg9v8pn1mek63skrf.apps.googleusercontent.com'
-//'D_En06NBoFANJtfAIZJ4xiWC'
-//'mongodb://kiranprod:password@ds245337.mlab.com:45337/emaily-prodk'
+//''
+//''
+//''
