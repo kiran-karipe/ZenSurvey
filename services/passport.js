cd303e96f79c9aae65283579db45a43d6dcd7ea5
Kiran <kiran.karipe@gmail.com>
2018-01-05 17:03:47 -0800

    Authentication

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy({
@@ -15,9 +25,12 @@ passport.use(
       .then((existingUser) => {
         if(existingUser) {
           // we already have record with the given profile Id
+          done(null, existingUser);
         } else {
           // we don't have user record, make a new record
-          new User({ googleId: profile.id}).save();
+          new User({ googleId: profile.id})
+          .save()
+          .then(user => done(null, user));
         }
       });
   })
